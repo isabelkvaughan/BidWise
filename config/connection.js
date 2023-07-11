a8479ad803d58d6bec9511cc71afd6c01cbc50d7
Isabel <isabelkvaughan@isabels-mbp.home>
2023-07-12 00:09:46 +0200

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
   port: process.env.DB_PORT,
